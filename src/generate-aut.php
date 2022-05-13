d3419cdf01ea9ce5546868e5a89e79b7a03e2bfa
Your Name <you@example.com>
2022-05-13 18:43:52 +0100

    version 1.0

--- src/generate-aut.php
+++ src/generate-aut.php
@@ -1,1 +1,19 @@
-c
+<?php
+    require_once 'vendor/autoload.php';
+    use PhpOffice\PhpWord\TemplateProcessor;
+    $templateProcessor = new TemplateProcessor('C:\xampp\htdocs\request-intership-docs\src\docs\Hello mester.docx');
+    $templateProcessor->setValue('stud_name', $_POST['entr-loc']);
+    $filepath = 'C:/xampp/htdocs/request-intership-docs/src/docs/result.docx';
+    ob_clean();
+    $templateProcessor->saveAS($filepath);
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    header('Content-Disposition: attachment; filename="autorisation.docx"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filepath));
+    readfile($filepath);
+    unlink($filepath) ;       
+    //save his thing
+?>
